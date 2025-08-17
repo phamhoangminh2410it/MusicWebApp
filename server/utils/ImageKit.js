e950538dae1f4c85952e9ddbb3e9f9a55e37268e
Minh <125272514+phamhoangminh2410it@users.noreply.github.com>
2025-08-18 06:26:53 +0700

    backend music app

--- server/utils/ImageKit.js
+++
@@ -1,9 +0,0 @@
-const ImageKit = require('imagekit');
-
-const imagekit = new ImageKit({
-  publicKey: process.env.IK_PUBLIC,
-  privateKey: process.env.IK_PRIVATE,
-  urlEndpoint: process.env.IK_URL,
-});
-
-module.exports = imagekit;
